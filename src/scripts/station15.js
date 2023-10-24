f1f71c70b428c3da1e1394472d3eaf61c6300e6a
takutou <takuto1488@gmail.com>
2023-10-24 15:34:01 +0900

    techtrain html_train_1　を終わらせました

--- src/scripts/station15.js
+++ src/scripts/station15.js
@@ -3,14 +3,36 @@ async function getData() {
   // ３秒後にフルネームのリストを表示されるようにしましょう。
   // 最初から表示されていると、クリアになりません。
   const userList = [
-    { id: 1, first_name: '優', family_name: '大木', affiliation: 'TechTrain', is_student: false },
-    { id: 2, first_name: '太郎', family_name: '山田', affiliation: 'HogeHoge大学', is_student: true }
+    {
+      id: 1,
+      first_name: "優",
+      family_name: "大木",
+      affiliation: "TechTrain",
+      is_student: false,
+    },
+    {
+      id: 2,
+      first_name: "太郎",
+      family_name: "山田",
+      affiliation: "HogeHoge大学",
+      is_student: true,
+    },
   ];
-  const result = await test()
-  return await result
+  const result = await new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(test(userList));
+    }, 3000);
+  });
+
+  return await result;
 }
 
-function test() {
-  return
+function test(userList) {
+  return userList.map(buildFullName);
 }
 
+function buildFullName(data) {
+  const tmp = data.family_name + " " + data.first_name;
+  data.full_name = tmp;
+  return data;
+}
